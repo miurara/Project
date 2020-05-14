dadff027b6fe5df24a7f4ebec694f6b6e4d6995d
Nata <miurara10@gmail.com>
2020-05-14 16:09:06 +0300

    first commit

---
+++ js/script - взаимодействие кода и пользователя.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// alert('hello');
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("Вам есть 18 лет?", "18");
+// console.log(typeof(answer));
+// const answer = +prompt("Вам есть 18 лет?", "18");
+// console.log(answer + 5);
+
+const answers = [];
+
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Как ваша фамилия?', '');
+answers[2] = prompt('Сколько вам лет?', '');
+
+console.log(typeof(answers));
---
+++ js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+let number = 5;
+
+console.log(-5/0);
+console.log('string' * 9);
+
+const persone = "Alex";
+
+const bool = false;
+
+let und;
+console.log(und);
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+console.log(obj.name);
+
+let arr = ['plam.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+console.log(arr[1]);
